6e366b2ddbe411fc1d21ce37920d6f12555b6973
qiptkami <1429711247@qq.com>
2022-09-14 21:58:57 +0800

    fix: 修改header bar less样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import TopBar from './components/sideBar/TopBar';
 import FooterBar from './components/sideBar/FooterBar';
-import './App.css';
 
 function App() {
   return (
--- src/components/sideBar/TopBar.tsx
+++ src/components/sideBar/TopBar.tsx
@@ -1,37 +1,32 @@
-import { HomeOutlined, TagsOutlined, BlockOutlined, InfoOutlined } from '@ant-design/icons';
+import { HomeOutlined, TagsOutlined, BlockOutlined, InfoOutlined, SearchOutlined } from '@ant-design/icons';
 import React from 'react';
+import "./TopBar.less"
 
 const TopBar: React.FC = () => {
   const handleSubmit = () => {
-
+    alert('Submit')
   }
   return (
-    <header className="ui inverted attached segment m-padded-tb-mini m-shadow-small">
-      <div className="ui container">
-      <div className="ui inverted secondary stackable menu"> 
-      {/* <!--stackable手机移动端相应，屏幕小时会将内容堆叠到一起--> */}
-        <h2 className="ui teal header item">Blog</h2>
-        <a href="@{/index}" className="{activeUri=='index'?'active m-item item m-mobile-hide':'m-item item m-mobile-hide'}">
-        <HomeOutlined />首页</a>
-        <a href="@{/types/-1}" className="{activeUri=='types'?'active m-item item m-mobile-hide':'m-item item m-mobile-hide'}">
-        <TagsOutlined />分类</a>
-        <a  href="@{/archives}" className="{activeUri=='archives'?'active m-item item m-mobile-hide':'m-item item m-mobile-hide'}">
-        <BlockOutlined />归档</a>
-        <a href="@{/about}" className="{activeUri=='about'?'active m-item item m-mobile-hide':'m-item item m-mobile-hide'}">
-        <InfoOutlined />关于我</a>
-        <div className="right m-item item m-mobile-hide">
-          <form name="search" action="@{/search}" method="post" target="_blank">
-            <div className="ui icon inverted input m-margin-tb-tiny">
-              <input type="text" name="query" placeholder="Search..." value="query"/>
-              <i onClick={handleSubmit} className="search icon link"></i>
-            </div>
-          </form>
+    <header className='header-container'>
+      <div className='header-menu'> {/* <!--stackable手机移动端相应，屏幕小时会将内容堆叠到一起--> */}
+        <h2 className='header-menu-title'>Blog</h2>
+        <a href='@{/index}' className='header-menu-index'>
+          <HomeOutlined />首页
+        </a>
+        <a href='@{/types/-1}' className='header-menu-types'>
+          <TagsOutlined />分类
+        </a>
+        <a href='@{/archives}' className='header-menu-archives'>
+          <BlockOutlined />归档
+        </a>
+        <a href='@{/about}' className='header-menu-about'>
+          <InfoOutlined />关于我
+        </a>
+        <div className='header-menu-search'>
+          <input className='header-menu-search-input' type='text' name='query' placeholder='Search...' value=''/>
+          <SearchOutlined className='header-menu-search-icon' onClick={handleSubmit} />
         </div>
       </div>
-  </div>
-    <a className="ui menu toggle black icon button m-right-top m-mobile-show">
-        <i className="sidebar icon"></i>
-    </a>
     </header>
   );
 }
