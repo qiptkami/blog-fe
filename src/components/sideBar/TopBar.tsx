3e9943162a9cfac0dd8171ce1cde113b891bdbbd
qiptkami <1429711247@qq.com>
2022-09-20 22:19:02 +0800

    feat: 配置svg

--- src/components/sideBar/TopBar.tsx
+++ src/components/sideBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import { HomeOutlined, TagsOutlined, BlockOutlined, InfoOutlined, SearchOutlined } from '@ant-design/icons';
+
 import React, { useState } from 'react';
 import "./TopBar.less"
 
@@ -16,16 +17,20 @@ const TopBar: React.FC = () => {
       <div className='header-menu'> {/* <!--stackable手机移动端相应，屏幕小时会将内容堆叠到一起--> */}
         <span className='header-menu-title'>Blog</span>
         <a href='@{/index}' className='header-menu-index'>
-          <HomeOutlined />首页
+          <HomeOutlined />
+          <span>首页</span>
         </a>
         <a href='@{/types/-1}' className='header-menu-types'>
-          <TagsOutlined />分类
+          <TagsOutlined />
+          <span>分类</span>
         </a>
         <a href='@{/archives}' className='header-menu-archives'>
-          <BlockOutlined />归档
+          <BlockOutlined />
+          <span>归档</span>
         </a>
         <a href='@{/about}' className='header-menu-about'>
-          <InfoOutlined />关于我
+          <InfoOutlined />
+          <span>关于我</span>
         </a>
       </div>
       <form className='header-menu-search' onSubmit={(e)=>handleSubmit(e)}>  
