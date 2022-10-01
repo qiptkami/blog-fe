3764a2dc9d2d98822f616ea07bf8dcc2316e2c57
qipt <1429711247@qq.com>
2022-10-01 16:53:43 +0800

    feat: 增加选中高亮 && 无法选中

--- src/pages/HomePage/components/MyPagination/index.tsx
+++ src/pages/HomePage/components/MyPagination/index.tsx
@@ -1,4 +1,5 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import classNames from "classnames";
 import React, { useEffect, useState } from "react";
 import "./index.less";
 
@@ -11,12 +12,14 @@ interface Props {
 const MyPagination: React.FC<Props> = ({ total, page, size }) => {
   const [pageNum, setPageNum] = useState<number>(1); //总页数
   const [pageList, setPageList] = useState<any>();
+  const [currentPage, setCurrentPage] = useState<number>(1);
   useEffect(() => {
     setPageNum(Math.ceil(total / size));
     numList();
-  }, [total, page, size, pageNum]);
+  }, [total, page, size, pageNum, currentPage]);
   const handleChange = (i: number) => {
     console.log("当前页：", i);
+    setCurrentPage(i);
   };
 
   const prePage = () => {
@@ -31,7 +34,11 @@ const MyPagination: React.FC<Props> = ({ total, page, size }) => {
       list.push(
         <li
           key={i}
-          className={page === i ? "pagination-li" : "pagination-li"}
+          className={
+            currentPage === i
+              ? classNames("pagination-li", "pagination-li-active")
+              : "pagination-li"
+          }
           onClick={() => handleChange(i)}
         >
           {i}
@@ -43,11 +50,13 @@ const MyPagination: React.FC<Props> = ({ total, page, size }) => {
   return (
     <div className="pagination-container">
       <LeftOutlined
+        className={currentPage === 1 ? "pagination-disabled" : ""}
         style={{ fontSize: "12px", fontWeight: "700" }}
         onClick={prePage}
       />
       <ul className="pagination-ul">{pageList}</ul>
       <RightOutlined
+        className={currentPage === pageNum ? "pagination-disabled" : ""}
         style={{ fontSize: "12px", fontWeight: "700" }}
         onClick={nextPage}
       />
