e7e7076e072b4bb800111460c6fd30855bedf39d
qipt <1429711247@qq.com>
2022-10-21 23:50:05 +0800

    define vaialbes

--- src/components/InfiniteScroll/index.tsx
+++ src/components/InfiniteScroll/index.tsx
@@ -1,6 +1,10 @@
 import "index.less";
+import { useState } from "react";
 
 const InfiniteScroll: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
   return <></>;
 };
 export default InfiniteScroll;
