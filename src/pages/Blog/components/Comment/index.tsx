cc25eacc5fa147f6dd028fe025619dc63588523f
qipt <1429711247@qq.com>
2022-11-09 16:27:33 +0800

    feat: 新增博客详情页面

---
+++ src/pages/Blog/components/Comment/index.tsx
@@ -0,0 +1,118 @@
+const Comment: React.FC = () => {
+  return (
+    <div className="ui attached segment ${!blog.commentAble}">
+      <div id="comment-container" className="ui teal segment">
+        <div className="ui threaded comments">
+          <h3 className="ui dividing header" style={{ maxWidth: "100%" }}>
+            评论
+          </h3>
+          <div className="comment   comment : ${comments}">
+            <a className="avatar">
+              <img src="${comment.avatar}" />
+            </a>
+            <div className="content">
+              <a className="author">
+                <span>${"nickname"}</span>
+                <div className="ui mini basic teal left pointing label m-padded-mini th:if=${comment.isAdminComment}"></div>
+              </a>
+              <div className="metadata">
+                <span className="date">
+                  "comment.createTime ,'yyyy-MM-dd HH:mm"
+                </span>
+              </div>
+              <div className="text">comment.content</div>
+              <div className="actions">
+                <a
+                  className="reply"
+                  data-commentid="1"
+                  data-commentnickname="yiqiandewo attr data-commentid=${comment.id}, data-commentnickname=${comment.nickname}"
+                  onClick={() => {
+                    console.log("reply(this)");
+                  }}
+                >
+                  回复
+                </a>
+              </div>
+            </div>
+            <div className="comments th:unless=${#arrays.length(comment.replyComment)} <= 0">
+              <div className="comment th:each=reply : ${comment.replyComment}">
+                <a className="avatar">
+                  <img src="${reply.avatar}" />
+                </a>
+                <div className="content">
+                  <a className="author">
+                    <span>reply.nickname</span>
+                    <div className="ui mini basic teal left pointing label m-padded-mini th:if=${reply.isAdminComment}"></div>
+                    <span className="m-teal">
+                      ' @' + reply.parentComment.nickname
+                    </span>
+                  </a>
+                  <div className="metadata">
+                    <span className="date">
+                      reply.createTime ,'yyyy-MM-dd HH:mm'
+                    </span>
+                  </div>
+                  <div className="text">reply.content</div>
+                  <div className="actions">
+                    <a
+                      className="reply"
+                      data-commentid="1"
+                      data-commentnickname="yiqiandewo  th:attr=data-commentid=${reply.id}, data-commentnickname=${reply.nickname}"
+                      onClick={() => {
+                        console.log("reply(this)");
+                      }}
+                    >
+                      回复
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui form">
+        <div className="field">
+          <textarea name="content" placeholder="请输入评论信息..."></textarea>
+          <input type="hidden" name="blog.id" value="${blog.id}" />
+          <input type="hidden" name="parentComment.id" value="-1" />
+        </div>
+        <div className="fields">
+          <div className="field m-mobile-wide m-margin-bottom-small">
+            <div className="ui left icon input">
+              <i className="user icon"></i>
+              <input
+                type="text"
+                name="nickname"
+                placeholder="姓名"
+                value="${session.user} != null ? ${session.user.username} : ''"
+              />
+            </div>
+          </div>
+          <div className="field m-mobile-wide m-margin-bottom-small">
+            <div className="ui left icon input">
+              <i className="mail icon"></i>
+              <input
+                type="text"
+                name="email"
+                placeholder="邮箱"
+                value="${session.user} != null ? ${session.user.email} : ''"
+              />
+            </div>
+          </div>
+          <div className="field m-mobile-wide m-margin-bottom-small">
+            <button
+              id="comment-post-btn"
+              type="button"
+              className="ui teal button m-mobile-wide"
+            >
+              <i className="edit icon"></i>发布
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
---
+++ src/pages/Blog/index.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import "./index.less";
+
+const BlogDetail: React.FC = () => {
+  return (
+    <div className="blog-detail">
+      <div className="ui container">
+        <div className="ui top attached segment">
+          <div className="ui horizontal link list" id="item1">
+            <div className="item">
+              <img className="ui avatar image" src="${blog.user.avatar}" />
+              <div className="content">
+                <a className="header" href="@{/about}">
+                  blog.user.username
+                </a>
+              </div>
+            </div>
+            <div className="item" id="item2">
+              <i className="clock icon"></i>
+              <span style={{ marginLeft: "5px", padding: "5px 0" }}>
+                blog.updateTime, 'yyyy-MM-dd'
+              </span>
+            </div>
+            <div className="item" id="item3">
+              <i className="eye icon"></i>
+              <span style={{ marginLeft: "5px", padding: "5px 0" }}>
+                blog.views
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="ui attached segment">
+          <img
+            className="ui fluid rounded image"
+            src="${blog.firstPicture}"
+            alt=""
+          />
+        </div>
+
+        <div className="ui attached padded segment">
+          <div className="ui right aligned basic segment">
+            <div className="ui orange basic label">blog.flag原创</div>
+          </div>
+          <h2 className="ui center aligned header">blog.title</h2>
+          <div
+            id="content"
+            className="typo typo-selection js-toc-content m-padded-lr-responsive m-padded-tb-large"
+          >
+            blog.content -- markdown
+          </div>
+          <div className="m-padded-lr-responsive m-padded-tb-large">
+            <div className="ui basic teal left pointing label">
+              blog.type.name
+            </div>
+          </div>
+          <div className="th:unless=${!blog.appreciation}">
+            <div className="ui center aligned basic segment">
+              <button
+                id="payButton"
+                className="ui orange basic circular button"
+              >
+                赞赏
+              </button>
+            </div>
+            <div className="ui payQR flowing popup top left transition hidden">
+              <div className="ui orange basic label">
+                <div className="ui images">
+                  <div className="image">
+                    <img
+                      src="@{/images/zfb.jpg}"
+                      alt=""
+                      className="ui rounded bordered image"
+                      style={{ width: "120px" }}
+                    />
+                    <div>支付宝</div>
+                  </div>
+                  <div className="image">
+                    <img
+                      src="@{/images/wx.png}"
+                      alt=""
+                      className="ui rounded bordered image"
+                      style={{ width: "120px" }}
+                    />
+                    <div>微信</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ui attached positive message th:if=${blog.shareStatement}">
+          <div className="ui middle aligned grid">
+            <div className="eleven wide column">
+              <ul className="list">
+                <li>
+                  作者：<span>blog.user.username</span>
+                  <a href="@{/about}" target="_blank">
+                    （联系作者）
+                  </a>
+                </li>
+                <li>
+                  发表时间：<span>blog.updateTime, 'yyyy-MM-dd HH:mm</span>
+                </li>
+                <li>
+                  版权声明：自由转载-非商用-非衍生-保持署名（创意共享3.0许可证）
+                </li>
+                <li>公众号转载：请在文末添加作者公众号二维码</li>
+              </ul>
+            </div>
+            <div className="five wide column">
+              <img
+                src="@{/images/MyWeChat.png}"
+                alt=""
+                className="ui right floated rounded bordered image"
+                style={{ width: "1120px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetail;
