54c19a79415f4f991d8f6522f285bdca03186f00
qipt <1429711247@qq.com>
2022-11-18 17:11:48 +0800

    feat: add marked parse markdown

--- src/pages/BlogDetail/components/MarkDown2Html/index.tsx
+++ src/pages/BlogDetail/components/MarkDown2Html/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { marked } from 'marked';
 
 interface Props {
   content?: string;
 }
 
 const MarkDown2Html: React.FC<Props> = ({ content }) => {
-  return <div className='blog-info'>{content}</div>;
+  return <div className='blog-info'>{content && marked.parse(content)}</div>;
 };
 
 export default MarkDown2Html;
