29f9ca6bac1a1370b2c802ee4cdecf2bec618237
qipt <1429711247@qq.com>
2022-11-19 23:33:03 +0800

    feat:  marked parser config

--- src/pages/BlogDetail/components/MarkDown2Html/index.tsx
+++ src/pages/BlogDetail/components/MarkDown2Html/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { marked } from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/base16/Atom-One-Dark.css'; //css 详见https://highlightjs.org/static/demo
 
 interface Props {
   content?: string;
 }
 
 const MarkDown2Html: React.FC<Props> = ({ content }) => {
-  return <div className='blog-info'>{content && marked.parse(content)}</div>;
+  const [markdownContent, setMarkdownContent] =
+    useState<string>('加载中。。。请稍后'); //html内容
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    },
+    gfm: true, // 允许 GitHub标准的markdown.
+    pedantic: false, // 不纠正原始模型任何的不良行为和错误（默认为false）
+    sanitize: false, // 对输出进行过滤（清理），将忽略任何已经输入的html代码（标签）
+    breaks: false, // 允许回车换行（该选项要求 gfm 为true）
+    //TODO 允许支持表格语法（该选项要求 gfm 为true）
+    smartLists: true, // 使用比原生markdown更时髦的列表
+    smartypants: false, // 使用更为时髦的标点
+  });
+
+  useEffect(() => {
+    const html =
+      content && marked(content).replace(/<pre>/g, "<pre class='hljs'>");
+    html && setMarkdownContent(html);
+  }, [content]);
+
+  return (
+    <div
+      className='blog-info'
+      dangerouslySetInnerHTML={{ __html: markdownContent }}
+    ></div>
+  );
 };
 
 export default MarkDown2Html;
